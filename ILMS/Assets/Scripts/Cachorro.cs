2894fda97c12425e62c455ced8d8f756756e7fec
Lfseibel <lfseibel@live.com>
2021-05-28 20:07:17 -0400

    Inimigo seguinda e mapa desenvolvendo

---
+++ ILMS/Assets/Scripts/Cachorro.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cachorro : EntityController
+{
+    // pursueRange  : Persegue player a partir dessa distância
+    // attackRange  : Ataca player a partir dessa distância
+    // retreatRange : Recua do player a partir dessa distância
+    [SerializeField] private float pursueRange, attackRange, retreatRange;
+
+    // Objeto do player usado como referência
+    // em diversas verificações
+    private GameObject player;
+
+    // Distância do inimigo ao player
+    protected float distPlayer;
+
+    // Direção do player, em coordenadas x
+    protected float playerDirection;
+
+    // Verifica o estado de movimento
+    // -1 - Idle | 2 - Aproximação | 1 - Ataque | 0 - Recuo
+    protected float enemyMovementStatus = -1;
+
+    // Threshold de movimento, essa lista é usada
+    // para evitar excessivos aninhamentos de if-elses
+    protected float[] enemyMovementThreshold = new float[3];
+
+    // Verifica se pode atacar
+    protected bool canAttack = true;
+
+    // Tempo de espera para criar outro projétil
+    [SerializeField] private float attackCooldown = 2f;
+
+    // Variável de contagem do tempo de espero
+    private float attackCooldownTimer;
+
+    // Awake é executado antes do Start
+    // base.Awake() copia os conteúdos
+    // de CharacterController : Awake ()
+    private new void Awake ()
+    {
+        base.Awake();
+
+        // Procurar o objeto do player na cena
+        // ATENÇÃO: As funções GameObject.Find e afins
+        // tem performance ruim, tenha certeza
+        // de não usá-las constantemente
+        player = GameObject.Find("Stalker");
+
+        // Cria uma lista de 'thresholds' the ações
+        // Imagem em anexo para visualizar
+        enemyMovementThreshold = new float[]{ retreatRange, attackRange, pursueRange };
+
+        // Inicializa o contador do ataque
+        attackCooldownTimer = attackCooldown;
+    }
+
+    // Update é chamado uma vez por frame
+    // base.Update() copia os conteúdos
+    // de CharacterController : Update ()
+    protected new void Update()
+    {
+        base.Update();
+
+        // Calcula a distância ao player
+        distPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if(distPlayer <= pursueRange)
+        {
+            MoveTowardsPlayer();
+        }
+           
+        
+    }
+
+    // Função genérica de movimentação
+    // isNotRetreat verifica se é aproximação
+    // ou recuo.
+    protected void MoveTowardsPlayer ()
+    {
+        Vector2 direction = transform.position - player.transform.position;
+        MovePosition(-direction);
+     
+    }
+
+
+
+}
--- ILMS/Assets/Scripts/EntityController.cs
+++ ILMS/Assets/Scripts/EntityController.cs
@@ -14,23 +14,12 @@ public class EntityController : MonoBehaviour
     // Precisamos do rigidbody para computar os movimentos
     protected Rigidbody2D rb;
 
-    // Precisamos do animator para ativar as transições entre as animações
-    private Animator anim;
 
-    // Verifica se o sprite está olhando para a direita ou não
-    private bool isFacingFront = true;
-
-    // Verifica se o player pode mexer ou não
-    private bool canMove = true;
 
     // Verifica se o player está atacando ou não
     protected bool isAttacking = false;
 
-    // Velocidade atual do objeto usado para cálculos
-    private float currentSpeed = 0f;
 
-    // Tempo para suavizar o movimento
-    [SerializeField] private float dampeningTime = 0.15f;
 
     // Objeto de ataque (players e inimigos)
     [SerializeField] GameObject attackObject;
@@ -40,65 +29,24 @@ public class EntityController : MonoBehaviour
     protected void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+       
     }
 
     // Update é chamado uma vez por frame
     protected void Update()
     {
-        // Atualiza as variáveis de controle do animator
-        anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
-        anim.SetBool("isAttacking", isAttacking);
+      
     }
 
     protected void MovePosition(Vector2 direction)
     {
         Vector2 actualPosition = (Vector2)rb.position;
-        rb.MovePosition(actualPosition + direction * Time.fixedDeltaTime * speed);
+        rb.MovePosition(actualPosition + direction.normalized * Time.fixedDeltaTime * speed);
     }
 
-    private void AdjustOrientation(Vector2 movement)
-    {
-        // Verifica a direção do movimento e 
-        // a qual direção o sprite está olhando
-        if (isFacingFront && movement.x < 0)
-        {
-            isFacingFront = false;
-            Flip();
 
-        }
 
-        if (!isFacingFront && movement.x > 0)
-        {
-            isFacingFront = true;
-            Flip();
-        }
-    }
+ 
 
-    private void Flip()
-    {
-        // Muda o sprite de direção dependendo do movimento
-        Vector3 objectScale = transform.localScale;
-        objectScale.y *= -1;
-        transform.localScale = objectScale;
-    }
 
-    protected void StopBody()
-    {
-        // Queremos manter a velocidade em y por conta da gravidade
-        // SmoothDamp suaviza o movimento com base em dampeningTime
-        Vector2 dampenedVelocity = new Vector2(Mathf.SmoothDamp(rb.velocity.x, 0f, ref currentSpeed, dampeningTime), rb.velocity.y);
-        rb.velocity = dampenedVelocity;
-    }
-
-    protected void StartAttack()
-    {
-        isAttacking = true;
-    }
-
-    public void PerformAttack()
-    {
-        isAttacking = false;
-        Debug.Log("Atacou!");
-    }
 }
